898092df8479b4e2c467635b7afde7e505b1e76b
Christophe <pro.chrisrln@gmail.com>
2020-10-09 11:52:24 +0200

    Chris : Add error message.

--- random-pass.py
+++ random-pass.py
@@ -5,6 +5,7 @@ import sys
 import random
 import string
 
+
 def CreatePassword(size, numbers, special_characters):
 
     password_length = size
@@ -13,14 +14,23 @@ def CreatePassword(size, numbers, special_characters):
     for i in range(0, password_length):
         password = password + random.choice(string.digits*numbers)
         numbers = numbers-1
-        password = password + random.choice(string.punctuation*special_characters)
+        password = password + \
+            random.choice(string.punctuation*special_characters)
         special_characters = special_characters-1
         password = password + random.choice(string.letters)
 
     return(password)
 
+
 def main():
-    print(CreatePassword(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3])))
+    try:
+        print(CreatePassword(int(sys.argv[1]), int(
+            sys.argv[2]), int(sys.argv[3])))
+
+    except (ValueError, IndexError):
+        print('\033[93m%s\x1b[0m' % "Ooops.. Please try : \n"
+              "python random-pass.py 'Size of your password' 'Number wanted' 'Special characters wanted'")
+
 
 if __name__ == "__main__":
     main()
